0f3f0595cf32bb84872d61e8342674b82c2802da
Hudson Carolino <hudson@kenzie.com.br>
2020-11-10 22:31:10 -0300

    adicionando formulario e efeito

--- js/geral.js
+++ js/geral.js
@@ -1,24 +1,77 @@
+(function(){
+
+	$(document).ready(function(){
 
 	
-	function sacar(valor){
-	
-		let notas =     [100,50,20,10,5,2];
-		let somaNotas = [0,  0,  0,0, 0,0]
-		for(let  i = 0; i < notas.length; i++){
-			if( valor >= notas[i] ){
-
-			
-				
-				somaNotas[i] = somaNotas[i]+1
-				valor = valor - notas[i]
-				
-
+		
+		$('.scrollTop a').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
 			}
 			
-		}
-		console.log(somaNotas);
-		
-	}
-	
-sacar(25)
+		});
 
+	
+
+		
+
+	});
+	
+	
+	
+	let labFormContact  = document.querySelector(".lab-form-contact")
+	let body            = document.querySelector("body")
+	let openFormContact = document.querySelector(".openFormContact")
+	let closeFormContact = document.querySelector(".lab-form-contact span.close")
+	
+	openFormContact.addEventListener("click",(e) => {
+		e.preventDefault()
+		body.classList.add("sombra")
+		labFormContact.classList.add("openForm")
+	});
+
+	closeFormContact.addEventListener("click",(e) => {
+		e.preventDefault()
+
+		body.classList.remove("sombra")
+		labFormContact.classList.remove("openForm")
+
+		
+	});
+
+
+	$('.pg-inicial section:nth-child(5) article.lab-container-full div.lab-col div.lab-content ul li').click(function(e){
+			let myThisp = $(this).children('p')
+			let myThis = $(this);
+
+
+			if(!$(this).hasClass('localizacao-active')){
+				$('.pg-inicial section:nth-child(5) article.lab-container-full div.lab-col div.lab-content ul li').removeClass('localizacao-active');
+				$('.pg-inicial section:nth-child(5) article.lab-container-full div.lab-col div.lab-content ul li p').removeClass('show-sobre');
+
+				$(this).addClass('localizacao-active');
+
+				setTimeout(function(){
+					myThisp.addClass('show-sobre');
+				}, 200);
+			} else{
+				myThisp.removeClass('show-sobre');
+
+				setTimeout(function(){
+					myThis.removeClass('localizacao-active');
+				}, 200);
+			}
+
+		});
+
+
+
+
+}());
